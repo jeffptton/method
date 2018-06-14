73a4fcc163988fb2dd6312fb35f2107ca7cf0665
jeffptton <39930443+jeffptton@users.noreply.github.com>
2018-06-14 15:16:37 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add(10, 8);
+            
+
+            Console.ReadKey();
+                  void Add (int num01 , int num02)
+
+                {
+                Console.WriteLine(num01 + num02);
+                
+                Console.ReadKey();
+                }
+            }
+        }
+    }
+
